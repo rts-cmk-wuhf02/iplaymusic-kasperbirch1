a6e7e05c468a97179399122ab946066109dc9adf
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-01-14 14:06:15 +0100

    updated

---
+++ src/js/change-theme.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    // console.log(document.documentElement.dataset.theme);
+
+    // Finder sidst valgte theme i localstorge
+    var currentTheme = localStorage.getItem('current-theme');
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    //  change theme 
+    const changeBtn = document.querySelector(".fa-cogs");
+    const bottomMenu = document.querySelector(".bottom-nav")
+    console.log(bottomMenu.style.backgroundColor);
+
+    changeBtn.addEventListener("click", () => {
+        if (document.documentElement.dataset.theme == "light") {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            // 
+            changeBtn.classList.add("dark-theme-activate");
+            localStorage.setItem('current-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            // 
+            changeBtn.classList.remove("dark-theme-activate");
+            localStorage.setItem('current-theme', 'white');
+        }
+    })
+});

b54aa9dbc9418c942e9d638def942f7e87bf6423
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-01-21 14:06:22 +0100

    Updated

--- dist/assets/js/get-data.js
+++ dist/assets/js/get-data.js
@@ -2,21 +2,44 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOMContentLoaded");
-  var URL = "https://jsonplaceholder.typicode.com/photos";
-  fetch(URL).then(function (respons) {
+  /* getToken and save ind sesision storage  */
+
+  (function getToken() {
+    var clientId = "dc0f55f09fdf4d578243f12ea258e145";
+    var ClientSecret = "5b2c30afe4c949feba3eab6f83b60973";
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + btoa(clientId + ":" + ClientSecret)
+      },
+      body: "grant_type=client_credentials"
+    }).then(function (respons) {
+      return respons.json();
+    }).then(function (resultat) {
+      return sessionStorage.setItem('token', "Bearer ".concat(resultat.access_token));
+    });
+  })();
+  /* fetch data  */
+
+
+  var URL = "https://api.spotify.com/v1/browse/categories";
+  var ACCESSTOKEN = sessionStorage.getItem('token');
+  console.log("ACCESSTOKEN", ACCESSTOKEN);
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": ACCESSTOKEN
+    }
+  }).then(function (respons) {
     return respons.json();
   }).then(function (resultat) {
-    console.log(resultat); // resultat.forEach(element => {
-    // // Template Variabler
-    // const container = document.querySelector("");
-    // const template = document.getElementById("");
-    // const clone = template.content.cloneNode(true);
-    // clone.querySelector("").src = element.images[0];
-    // clone.querySelector("").innerText = element.make;
-    // clone.querySelector("").innerText = element.price;
-    // clone.querySelector("").href = `/product/?sku=${element.sku}`;
-    // // Tilføjer clone
-    // container.appendChild(clone);
-    // }
+    console.log(resultat);
+    console.log("resultat.categories.items", resultat.categories.items);
+    resultat.categories.items.forEach(function (element) {
+      console.log(element);
+    });
   });
 });
--- src/js/get-data.js
+++ src/js/get-data.js
@@ -1,28 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded");
 
-    const URL = "https://jsonplaceholder.typicode.com/photos";
+    /* getToken and save ind sesision storage  */
+    (function getToken() {
+        const clientId = "dc0f55f09fdf4d578243f12ea258e145";
+        const ClientSecret = "5b2c30afe4c949feba3eab6f83b60973";
 
-    fetch(URL)
+        fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + btoa(clientId + ":" + ClientSecret)
+            },
+            body: "grant_type=client_credentials"
+        })
+            .then(respons => respons.json())
+            .then(resultat => sessionStorage.setItem('token', `Bearer ${resultat.access_token}`))
+    })()
+
+
+    /* fetch data  */
+    const URL = "https://api.spotify.com/v1/browse/categories";
+    const ACCESSTOKEN = sessionStorage.getItem('token');
+    console.log("ACCESSTOKEN", ACCESSTOKEN);
+
+
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": ACCESSTOKEN
+        }
+
+    })
         .then((respons) => respons.json())
         .then((resultat) => {
             console.log(resultat)
-            // resultat.forEach(element => {
-            // // Template Variabler
-            // const container = document.querySelector("");
-            // const template = document.getElementById("");
-            // const clone = template.content.cloneNode(true);
+            console.log("resultat.categories.items", resultat.categories.items)
+            resultat.categories.items.forEach(element => {
+                console.log(element);
 
-            // clone.querySelector("").src = element.images[0];
-            // clone.querySelector("").innerText = element.make;
-            // clone.querySelector("").innerText = element.price;
-            // clone.querySelector("").href = `/product/?sku=${element.sku}`;
 
-            // // Tilføjer clone
-            // container.appendChild(clone);
-            // }
+            });
         });
-});
 
-
-
+})

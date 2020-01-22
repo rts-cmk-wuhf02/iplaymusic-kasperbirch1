7d0e2686045721c8396fa049da8bd2c7f8d4977b
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-01-22 09:22:49 +0100

    updated

--- src/js/get-featured.js
+++ src/js/get-featured.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const URL = "https://api.spotify.com/v1/browse/categories";
+    const URL = "https://api.spotify.com/v1/browse/featured-playlists";
     const ACCESSTOKEN = sessionStorage.getItem('token');
     console.log("ACCESSTOKEN", ACCESSTOKEN);
-
 
     fetch(URL, {
         method: "GET",
@@ -15,12 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
     })
         .then((respons) => respons.json())
         .then((resultat) => {
-            console.log(resultat)
-            console.log("resultat.categories.items", resultat.categories.items)
-            resultat.categories.items.forEach(element => {
-                console.log(element);
 
-
-            });
+            if (resultat.error = 401) {
+                createToken()
+            } else {
+                console.log(resultat)
+                console.log(resultat.playlists.items)
+                resultat.playlists.items.forEach(element => {
+                    console.log(element.name);
+                });
+            }
         });
 });
---
+++ src/js/get-spotify-token.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded");
+
+    if (sessionStorage.getItem("token") === null) {
+        createToken()
+    }
+
+    /* createToken and save ind sesision storage  */
+    function createToken() {
+        const clientId = "dc0f55f09fdf4d578243f12ea258e145";
+        const ClientSecret = "5b2c30afe4c949feba3eab6f83b60973";
+
+        fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + btoa(clientId + ":" + ClientSecret)
+            },
+            body: "grant_type=client_credentials"
+        })
+            .then(respons => respons.json())
+            .then(resultat => sessionStorage.setItem('token', `Bearer ${resultat.access_token}`))
+    }
+})
